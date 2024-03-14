3336527937f3d7f0dc3236202e49d4a8904cb008
Ravikiran B <ravikiran.basavaraju@fbmsales.com>
2024-03-14 18:51:29 +0530

    First Commit

---
+++ iOS-POC/POC/POC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC
+//
+//  Created by Ravikiran Basavaraju on 14/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-POC/POC/POC/POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCApp.swift
+//  POC
+//
+//  Created by Ravikiran Basavaraju on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
